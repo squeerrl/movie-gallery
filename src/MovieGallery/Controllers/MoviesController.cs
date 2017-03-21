a746a7e86fa4ec7558326dc0767a3f7367529139
Magdalena <magda.stallinger@gmail.com>
2017-03-21 17:18:20 +0100

    Funkt alles. Updates

---
+++ src/MovieGallery/Controllers/MoviesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieGallery.Models;
+
+namespace MovieGallery.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Detail()
+        {
+            var movie = new Movie()
+            {
+                MovieTitle = "Sunshine",
+                Year = 2007,
+                DescriptionHtml = "<p>Lot of fun, at the end <b>drunk</b>! Why?",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name = "Danny Boyle", Role = "Director"},
+                    new Artist() {Name = "Cillian Murphy", Role = "Actor"},
+                    new Artist() {Name = "Rose Byrne", Role = "Actor"},
+                    new Artist() {Name = "Chris Evans", Role = "Actor"},
+                    new Artist() {Name = "Alex Garland", Role = "Writing"}
+                }
+
+            };
+            //movie.MovieTitle = "BlaBla";
+            
+
+
+            return View(movie);
+            //return Redirect("/"); <-- weiterleitung zur startseite 
+            
+            //return Content("Hallo hallo ich hier");
+            /*=return new ContentResult()
+            {
+                Content = "Hallo hallo ich bina"
+            };
+            */
+        }
+    }
+}
--- src/MovieGallery/Models/Movie.cs
+++ src/MovieGallery/Models/Movie.cs
@@ -8,10 +8,30 @@ namespace MovieGallery.Models
     public class Movie
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string MovieTitle { get; set; }
         public int Year { get; set; }
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return MovieTitle + " (" + Year +")";
+            }
+        }
+
+        // Bildname: movie-year.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                //The Sunshine wird mittels replace(,) zu the-sunshine
+                return MovieTitle.Replace(" ", "-").ToLower() + "-" + Year + ".jpg";
+            }
+        }
+
+
     }
 }
