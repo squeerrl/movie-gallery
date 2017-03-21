3e55b75aa39392656969c8532d00e0d19daae056
Magdalena <magda.stallinger@gmail.com>
2017-03-21 15:55:03 +0100

    Model: Artist und Movie erstellt

---
+++ src/MovieGallery/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieGallery.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
